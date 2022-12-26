f146252d69fe59cc8475fdab8c38d523e20eb79b
hsbmaulana <hsbmaulana.iso@protonmail.com>
2022-12-26 23:15:45 +0700

    Initial Codebase

---
+++ index.vue
@@ -0,0 +1,97 @@
+<template>
+    <div></div>
+</template>
+
+<style scoped>
+
+div {
+
+    margin: 0;
+    padding: 0;
+}
+
+</style>
+
+<script>
+
+/**
+ * @Object
+ */
+export default {
+
+    /**
+     * @returns {Object}
+     */
+    data ()
+    {
+        return {};
+    },
+
+    /**
+     * @returns {Object}
+     */
+    methods:
+    {},
+
+    /**
+     * @returns {Object}
+     */
+    computed:
+    {},
+
+    /**
+     * @returns {Object}
+     */
+    watch:
+    {},
+
+    /**
+     * @returns {void}
+     */
+    beforeCreate ()
+    {},
+
+    /**
+     * @returns {void}
+     */
+    created ()
+    {},
+
+    /**
+     * @returns {void}
+     */
+    beforeMount ()
+    {},
+
+    /**
+     * @returns {void}
+     */
+    mounted ()
+    {},
+
+    /**
+     * @returns {void}
+     */
+    beforeUpdate ()
+    {},
+
+    /**
+     * @returns {void}
+     */
+    updated ()
+    {},
+
+    /**
+     * @returns {void}
+     */
+    beforeUnmount ()
+    {},
+
+    /**
+     * @returns {void}
+     */
+    unmounted ()
+    {},
+};
+
+<script>
